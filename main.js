42a37fc61d2e222a51c981ba5a5bbdcb5aa96efa
skylarbeese <skylarbeese@gmail.com>
2020-10-27 13:28:42 -0600

    commit new

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
 import { films } from './data/films.js'
+import { people } from './data/people.js'
+
+
+//console.log(films.length)
+
+
+const titles = document.querySelector('.titles')
+const movies = document.querySelector('.movies')
+films.forEach(film => {
+   
+  
+    let titleCon = document.createElement('div')
+    let titleCrawl = document.createElement('p')
+    let newTitle = document.createElement('h1') 
+  
+    for(let i = 0; i < 7; i++) {
+      
+        let poster = document.createElement('img')    
+        poster.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+        
+        titleCon.appendChild(poster)
+     
+        } 
+    
+        
+        titleCon.appendChild(newTitle)
+        titleCon.setAttribute("class", "movie")
+        titleCon.appendChild(titleCrawl)
+        titleCrawl.textContent = film.opening_crawl
+        newTitle.textContent = film.title
+        
+        movies.appendChild(titleCon)
+       
+     
+    
+
+
+       
+        
+})
+
+
+
+
+
